0d3f6fdba5991751883b0aec875a1c28fdce0e4c
Mateo Luque <mateoluque@aol.com>
2026-06-18 13:33:30 +0200

    fix: restore users count on navigation and setup-uv version-file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import { countUsers } from './api/api';
 import AdminRoute from './components/AdminRoute/AdminRoute';
@@ -11,6 +11,7 @@ import ListPage from './pages/ListPage';
 import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
+  const location = useLocation();
   const [usersCount, setUsersCount] = useState(0);
 
   useEffect(() => {
@@ -35,7 +36,7 @@ function App() {
     return () => {
       isMounted = false;
     };
-  }, []);
+  }, [location]);
 
   return (
     <div className="App">
