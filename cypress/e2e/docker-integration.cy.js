13db0855cedee2d1b4ad6c93944a3fd745840663
Mateo Luque <mateoluque@aol.com>
2026-06-18 09:52:20 +0200

    fix(ci): reset DB and wait for API in Docker E2E

--- cypress/e2e/docker-integration.cy.js
+++ cypress/e2e/docker-integration.cy.js
@@ -1,18 +1,26 @@
 describe('Integration Docker', () => {
   it('affiche 0 inscrit au demarrage', () => {
+    cy.intercept('GET', '**/users').as('getUsers');
     cy.visitHomeDocker();
+    cy.wait('@getUsers');
     cy.assertHomePage();
     cy.assertRegisteredUsersCount(0);
   });
 
   it('inscrit un utilisateur via l api reelle', () => {
     cy.fixture('users').then(({ validUser }) => {
+      cy.intercept('POST', '**/users').as('createUser');
+      cy.intercept('GET', '**/users').as('getUsers');
+
       cy.visitHomeDocker();
+      cy.wait('@getUsers');
       cy.goToRegistrationForm();
       cy.fillRegistrationForm(validUser);
       cy.submitRegistrationForm();
 
+      cy.wait('@createUser');
       cy.url().should('include', '/list');
+      cy.wait('@getUsers');
       cy.assertRegistrationListCount(1);
       cy.assertRegistrationVisible(validUser);
 
